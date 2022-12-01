0cc936b965b4cfc87436065d03364c8d6941e117
Juan Martinez <jumartinez@nowoptics.com>
2022-12-01 10:07:35 -0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {FrequentlyAskedQuestion, RemoteSupport, HearingAidsSuited, HearDigits, HearingLoss, Hero, ProductListing, ReviewCarousel} from '@hearsmart/website-nowoptics-my-eye-lab';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Hero />
+        <ProductListing />
+      </div>
     </div>
   );
 }
